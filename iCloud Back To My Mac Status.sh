e831339db627ed8e62462dee991eebca082ba24d
bp88 <bpavlov@BalmesPavlovsLoanerMacBookPro15.local>
2016-09-28 15:01:08 -0400

    Remove unneeded code

--- iCloud Back To My Mac Status.sh
+++ iCloud Back To My Mac Status.sh
@@ -4,34 +4,26 @@
 # If Back To My Mac has been setup previously then values should be: "false" or "true"
 # If the iCloud account has NOT been setup then value will be: "iCloud Account Disabled"
 
-#Variable to determine major OS version
-OSver="$(/usr/bin/sw_vers -productVersion | /usr/bin/cut -d . -f 2)"
+#Path to PlistBuddy
+plistBud="/usr/libexec/PlistBuddy"
 
-#Determine OS is 10.12 or greater as Doc Sync is only available on 10.12+
-if [ "$OSver" -ge "12" ]; then
-	#Path to PlistBuddy
-	plistBud="/usr/libexec/PlistBuddy"
+#Determine logged in user
+loggedInUser=$(python -c 'from SystemConfiguration import SCDynamicStoreCopyConsoleUser; import sys; username = (SCDynamicStoreCopyConsoleUser(None, None, None) or [None])[0]; username = [username,""][username in [u"loginwindow", None, u""]]; sys.stdout.write(username + "\n");')
 
-	#Determine logged in user
-	loggedInUser=$(python -c 'from SystemConfiguration import SCDynamicStoreCopyConsoleUser; import sys; username = (SCDynamicStoreCopyConsoleUser(None, None, None) or [None])[0]; username = [username,""][username in [u"loginwindow", None, u""]]; sys.stdout.write(username + "\n");')
+#Determine whether user is logged into iCloud
+if [[ -e "/Users/$loggedInUser/Library/Preferences/MobileMeAccounts.plist" ]]; then
+	iCloudStatus=$("$plistBud" -c "print :Accounts:0:LoggedIn" /Users/$loggedInUser/Library/Preferences/MobileMeAccounts.plist 2> /dev/null )
 
-	#Determine whether user is logged into iCloud
-	if [[ -e "/Users/$loggedInUser/Library/Preferences/MobileMeAccounts.plist" ]]; then
-		iCloudStatus=$("$plistBud" -c "print :Accounts:0:LoggedIn" /Users/$loggedInUser/Library/Preferences/MobileMeAccounts.plist 2> /dev/null )
-
-		#Determine whether user has enabled Drive enabled. Value should be either "false" or "true"
-		if [[ "$iCloudStatus" = "true" ]]; then
-			BackToMyMacStatus=$("$plistBud" -c "print :Accounts:0:Services:12:Enabled" /Users/$loggedInUser/Library/Preferences/MobileMeAccounts.plist 2> /dev/null )
-			if [ -z "$BackToMyMacStatus" ]; then
-				BackToMyMacStatus="iCloud Account Enabled, Back To My Mac Disabled"
-			fi
-		fi
-		if [[ "$iCloudStatus" = "false" ]] || [[ -z "$iCloudStatus" ]]; then
-			BackToMyMacStatus="iCloud Account Disabled"
+	#Determine whether user has enabled Drive enabled. Value should be either "false" or "true"
+	if [[ "$iCloudStatus" = "true" ]]; then
+		BackToMyMacStatus=$("$plistBud" -c "print :Accounts:0:Services:12:Enabled" /Users/$loggedInUser/Library/Preferences/MobileMeAccounts.plist 2> /dev/null )
+		if [ -z "$BackToMyMacStatus" ]; then
+			BackToMyMacStatus="iCloud Account Enabled, Back To My Mac Disabled"
 		fi
 	fi
-else
-	BackToMyMacStatus="OS Not Supported"
+	if [[ "$iCloudStatus" = "false" ]] || [[ -z "$iCloudStatus" ]]; then
+		BackToMyMacStatus="iCloud Account Disabled"
+	fi
 fi
 
 /bin/echo "<result>$BackToMyMacStatus</result>"
